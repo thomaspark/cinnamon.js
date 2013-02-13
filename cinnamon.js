02235f5d0eaeb4a436b77818d6c317adef00074a
Thomas Park <thomas@thomaspark.me>
2013-02-12 21:24:01 -0500

    reverting to font-size

--- cinnamon.js
+++ cinnamon.js
@@ -7,17 +7,22 @@
 
     // Add styles
     var overflow = 'hidden',
+        fontsize = '999px',
         browser;
 
     if ((navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1)) {
-        browser = 'safari';
         overflow = 'visible';
+        fontsize = 'inherit';
+        browser = 'safari';
+    } else if (navigator.userAgent.indexOf('Firefox') !== -1) {
+        browser = 'firefox';
+        fontsize = 'inherit';
     }
 
     var head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style'),
         css = '[data-cinnamon] { position: relative; display: inline-block; } \
-.cinnamon { z-index: -1; position: absolute; top: 0; left: 0; display: inline-block; height: 100%; width: 100%; overflow: ' + overflow + '; color: transparent; font-size: 9999px\\9; } \
+.cinnamon { z-index: -1; position: absolute; top: 0; left: 0; display: inline-block; height: 100%; width: 100%; overflow: ' + overflow + '; color: transparent; font-size: ' + fontsize + '; } \
 @media all and (device-width: 768px) and (device-height: 1024px) { .cinnamon { z-index: 1; opacity: 0.25; } }';
 
     style.type = 'text/css';
@@ -48,8 +53,8 @@
             e.className = 'cinnamon';
             e.setAttribute('aria-hidden', 'true');
 
-            if (image) {
-                e.style.lineHeight = image.height + 10 + 'px';
+            if (image && browser == 'firefox') {
+                e.style.fontSize = image.height + 20 + 'px';
             }
 
             if (typeof (e.textContent) !== "undefined") {
